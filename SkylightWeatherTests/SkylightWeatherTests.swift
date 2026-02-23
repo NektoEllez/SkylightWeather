8d4fe9032807618f1dc6087d1fd7ac1be7d683ec
Ivan Potyng <nekto.ellez@gmail.com>
2026-02-23 17:22:15 +0300

    Initial Commit

---
+++ SkylightWeatherTests/SkylightWeatherTests.swift
@@ -0,0 +1,16 @@
+//
+//  SkylightWeatherTests.swift
+//  SkylightWeatherTests
+//
+//  Created by Nekto_Ellez on 23.02.2026.
+//
+
+import Testing
+
+struct SkylightWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
